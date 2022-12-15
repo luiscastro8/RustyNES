60357fe044f5e995c2664bbfbf4936bd71ee3583
luiscastro8 <ochoad64@gmail.com>
2022-12-14 21:26:56 -0600

    implemented instruction

--- src/cpu.rs
+++ src/cpu.rs
@@ -27,6 +27,27 @@ impl CPU {
             self.pc += 1;
 
             match opcode {
+                0x00 => {
+                    // TODO: implement properly
+                    return;
+                }
+                0xa9 => {
+                    let param = program[self.pc as usize];
+                    self.pc += 1;
+                    self.a = param;
+
+                    if self.a == 0 {
+                        self.status |= 0b0000_0010;
+                    } else {
+                        self.status &= 0b1111_1101;
+                    }
+
+                    if self.a & 0b1000_0000 == 0b1000_0000 {
+                        self.status |= 0b1000_0000;
+                    } else {
+                        self.status &= 0b0111_1111;
+                    }
+                }
                 _ => todo!()
             }
         }
